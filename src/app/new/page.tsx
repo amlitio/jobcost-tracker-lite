34bffcce1c250a6798d3c7974b2fa7de40a1f245
kleos <36254493+amlitio@users.noreply.github.com>
2025-09-18 07:37:22 -0400

    Update page.tsx

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,1 +1,5 @@
+"use client";
+import AuthGate from "@/components/AuthGate";
+import JobForm from "@/components/JobForm";
+export default function NewJobPage(){ return <AuthGate><JobForm/></AuthGate>; }
 
